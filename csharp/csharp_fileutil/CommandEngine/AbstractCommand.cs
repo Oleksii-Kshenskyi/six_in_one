d3dfca7115de1e0d84087b16c1cb3ce58d323836
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2018-10-15 21:32:58 +0300

    Optimizing C# file util: - Transferring system commands away from the abstract command class into their separate file.

--- csharp/csharp_fileutil/CommandEngine/AbstractCommand.cs
+++ csharp/csharp_fileutil/CommandEngine/AbstractCommand.cs
@@ -25,30 +25,4 @@ namespace csharp_fileutil.CommandEngine
 
         public abstract void Execute();
     }
-
-    public class UsageCommand : AbstractCommand
-    {
-        public UsageCommand(List<string> args) : base(args)
-        {
-
-        }
-
-        public override void Execute()
-        {
-            Console.WriteLine("Available commands: {0}.", GetAvailableCommands());
-        }
-    }
-
-    public class ExitCommand : AbstractCommand
-    {
-        public ExitCommand(List<string> args) : base(args)
-        {
-
-        }
-
-        public override void Execute()
-        {
-            Environment.Exit(0);
-        }
-    }
 }
---
+++ csharp/csharp_fileutil/CommandEngine/SystemCommands.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_fileutil.CommandEngine
+{
+    public class UsageCommand : AbstractCommand
+    {
+        public UsageCommand(List<string> args) : base(args)
+        {
+
+        }
+
+        public override void Execute()
+        {
+            Console.WriteLine("Available commands: {0}.", GetAvailableCommands());
+        }
+    }
+
+    public class ExitCommand : AbstractCommand
+    {
+        public ExitCommand(List<string> args) : base(args)
+        {
+
+        }
+
+        public override void Execute()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
