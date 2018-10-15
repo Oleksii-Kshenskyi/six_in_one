362da3a2dd3bc4113c55c71fa751aa4e65713d57
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2018-10-16 00:05:57 +0300

    C# file util: - Renaming of the project and the namespace in preparation for the command engine migration.

--- csharp/csharp_fileutil/CommandEngine/AbstractCommand.cs
+++ csharp/csharp_fileutil/CommandEngine/AbstractCommand.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace csharp_fileutil.CommandEngine
+namespace CsharpFileutil.CommandEngine
 {
     public abstract class AbstractCommand
     {
--- csharp/csharp_fileutil/CommandEngine/CommandFactory.cs
+++ csharp/csharp_fileutil/CommandEngine/CommandFactory.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace csharp_fileutil.CommandEngine
+namespace CsharpFileutil.CommandEngine
 {
     class CommandFactory
     {
