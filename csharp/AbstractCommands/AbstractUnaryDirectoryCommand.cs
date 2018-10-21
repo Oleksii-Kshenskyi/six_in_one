4d573b945a8b0c3b0c242aec2d75473c53bf17b0
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2018-10-21 23:22:28 +0300

    C# file util READY! - Refactored the list command.

--- csharp/AbstractCommands/AbstractUnaryDirectoryCommand.cs
+++ csharp/AbstractCommands/AbstractUnaryDirectoryCommand.cs
@@ -10,6 +10,9 @@ namespace CsharpCommandEngine
         private const string ArgumentCountMismatchMessage = "ERROR: the command takes EXACTLY 1 argument!";
         private const string StickToSyntaxMessage = "ERROR: The argument has to be the name of the directory.";
 
+        protected List<string> Files { get; private set; }
+        protected List<string> Directories { get; private set; }
+
         protected override void SetupValidation()
         {
             Validation.AddValidator(new ArgumentCountValidator(Arguments, 1, ArgumentCountMismatchMessage));
@@ -19,6 +22,17 @@ namespace CsharpCommandEngine
         public AbstractUnaryDirectoryCommand(List<string> args) : base(args)
         {
             SetupValidation();
+
+            if (Arguments.Count == 1 && Directory.Exists(Arguments[0]))
+            {
+                Directories = new List<string>(Directory.GetDirectories(Arguments[0]));
+                Files = new List<string>(Directory.GetFiles(Arguments[0]));
+            }
+            else
+            {
+                Directories = null;
+                Files = null;
+            }
         }
     }
 }
--- csharp/CsharpFileCommands/ListDirectoryCommand.cs
+++ csharp/CsharpFileCommands/ListDirectoryCommand.cs
@@ -10,34 +10,13 @@ namespace CsharpFileCommands
         public new static string UsageString = "list command lists all the contents of <directory1>.\n" +
                                                "Use it the following way:\n" +
                                                "\t'list <directory1>'\n" +
-                                               "\tOutput is of the following format:" +
-                                               "\t'=[F|D] <[file|directory]_name> [(<file_size>)]'" +
-                                               "\twhere [F|D] shows if it is a file or a directory." +
+                                               "\tOutput is of the following format:\n" +
+                                               "\t'=[F|D] <[file|directory]_name> [(<file_size>)]'\n" +
+                                               "\twhere [F|D] shows if it is a file or a directory.\n" +
                                                "\tNOTE: the command takes EXACTLY 1 argument.";
 
         public ListDirectoryCommand(List<string> args) : base(args)
         {
-        }
-
-        public override void Execute()
-        {
-            if (!Validation.Validate())
-                return;
-
-            var dirs = Directory.GetDirectories(Arguments[0]);
-            var files = Directory.GetFiles(Arguments[0]);
-
-            if(dirs != null)
-                foreach (var item in dirs)
-                    Console.WriteLine("=D {0}", item);
-
-            if(files != null)
-                foreach (var item in files)
-                    Console.WriteLine("-F {0} ({1})", item, GetSize(item));
-
-            var dircount = dirs != null ? dirs.Length : 0;
-            var filescount = files != null ? files.Length : 0;
-            Console.WriteLine("======\n=TOTAL ITEMS: {0}.", dircount + filescount);
         }
 
         private string GetSize(string filename)
@@ -49,5 +28,36 @@ namespace CsharpFileCommands
                    (rawSize >= 1024 * 1024) ? string.Format("{0}MB", rawSize / (1024 * 1024)) : "";
         }
 
+        private void ListDirectories()
+        {
+            if (Directories != null)
+                foreach (var item in Directories)
+                    Console.WriteLine("=D {0}", item);
+        }
+
+        private void ListFiles()
+        {
+            if (Files != null)
+                foreach (var item in Files)
+                    Console.WriteLine("-F {0} ({1})", item, GetSize(item));
+        }
+
+        private void CountEntities()
+        {
+            var dircount = Directories != null ? Directories.Count : 0;
+            var filescount = Files != null ? Files.Count : 0;
+            Console.WriteLine("======\n=TOTAL ITEMS: {0}.", dircount + filescount);
+        }
+
+        public override void Execute()
+        {
+            if (!Validation.Validate())
+                return;
+
+            ListDirectories();
+            ListFiles();
+
+            CountEntities();
+        }
     }
 }
