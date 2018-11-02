27e2b83bf81a5eee6aa707b8c799f74c04ffd553
Oleksii Kshenskyi <oleksii.kshenskyi@outlook.com>
2018-11-02 16:49:05 +0200

    Python fileutil: - Extremely crude implementation of importing settings from JSON files. To be heavily refactored. TODO: - There's still no masked input for files - There's no proper formatted output (to console or to CSV file) - There's no validators of any kind in the lookup command - "with" statement doesn't work for some reason. Research why and refactor the if/else blocks. - Directory name to look up in should also be taken from JSON (duck typing?)

--- python/PythonSearchCommands/LookupCommand.py
+++ python/PythonSearchCommands/LookupCommand.py
@@ -1,4 +1,5 @@
 from AbstractCommands.AbstractDirectoryTraversalCommand import *
+import json
 
 
 class LookupCommand(AbstractDirectoryTraversalCommand):
@@ -7,32 +8,38 @@ class LookupCommand(AbstractDirectoryTraversalCommand):
         return "lookup command tries to find the list of your queries in a directory subtree.\n" + \
                "it returns a dictionary with the search results.\n" + \
                "use it the following way:\n" + \
-               "\t'lookup <directory1>'\n" + \
+               "\t'lookup <directory1> <config1>'\n" + \
                "\t" + cls._note_preface() + "WORK IN PROGRESS!"
 
     def __init__(self, args):
         super().__init__(args)
-        self._queries = ["I'm John BTW!@\r\nkek", "osatoshikun"]
-        self._results = {query: [] for query in self._queries}
+        self._results = {}
+        try:
+            jfile = open(self.arguments[1], "r") if self.arguments.__len__() > 0 else None
+            config = json.load(jfile) if jfile else None
+            if jfile:
+                jfile.close()
+            self._queries = config["queries"] if config else []
+            self._results = {query: [] for query in self._queries} if config else {}
+        except Exception as err:
+            print("Lookup exception while loading JSON: " + err.__str__())
 
     @staticmethod
     def find_in_file(filename, query, start=0):
         with open(filename, 'rb') as file:
             filesize = os.path.getsize(filename)
             bsize = 4096 if query.__len__() < 4096 else int(query.__len__() * 1.2)
-
             if start > 0:
                 file.seek(start)
             overlap = len(query) - 1
             while True:
                 tellpoint = file.tell()
-                if overlap <= tellpoint < filesize:  # tellpoint < filesize and tellpoint >= overlap:
+                if overlap <= tellpoint < filesize:
                     file.seek(tellpoint - overlap)
 
                 buffer = file.read(bsize)
                 if buffer != b'' or buffer:
                     pos = buffer.find(bytes(query, 'utf8'))
-
                     if pos >= 0:
                         return file.tell() - (len(buffer) - pos)
                 else:
