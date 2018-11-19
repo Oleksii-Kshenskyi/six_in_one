0a48aa05ea808b869cd8b0c8d31159522ed5dff8
Oleksii Kshenskyi <oleksii.kshenskyi@outlook.com>
2018-11-19 15:09:10 +0200

    Python fileutil: - Positional argument exception fixed. TODO: - Check if move&rename commands move files to folders and if yes, remove comments in AbstractBinaryFileCommand - There's still a problem with spaces in command arguments

--- python/PythonCommandEngine/NoneCommand.py
+++ python/PythonCommandEngine/NoneCommand.py
@@ -2,8 +2,8 @@ from .UsageCommand import *
 
 
 class NoneCommand(AbstractCommand):
-    def __init__(self):
-        super().__init__([])
+    def __init__(self, args):
+        super().__init__(args)
 
     def execute(self):
         print("Command not found.\n\t", sep='', end='', flush=True)
